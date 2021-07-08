71bf93dcad613845dd9428e803ee0d180fe1d585
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2021-07-07 21:58:24 -0400

    changes

--- express-todo/index.js
+++ express-todo/index.js
@@ -12,44 +12,12 @@ app.set('views', './views')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('../lobby/lobby')
 })
 
 app.get('/users', (req, res) => {
   // res.render('index', { currentTime: new Date() })
-  res.render('users', { users })
-})
-
-app.post('/users', (req, res) => {
-  // console.log(req.body)
-  const {name, password} = req.body
-  users.push({
-    id: idIndex++,
-    name,
-    password,
-    createdAt: new Date(),
-  })
-  console.log(users)
-
-  // res.send('Got it!')
-  // res.render('users', { users })
-  res.redirect(301, '/users')
-})
-
-app.post('/users/:id', (req, res) => {
-  console.log('id: ', req.params.id)
-  console.log('checkStatus: ', req.body.checkStatus)
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].id == req.params.id) {
-      console.log('user found!')
-      if (req.body.checkStatus == 'checked') {
-        users[i].done = true
-      } else {
-        users[i].done = false
-      }
-    }
-  }
-  res.send('OK')
+  res.render('todo', { users })
 })
 
 app.listen(port, () => {
