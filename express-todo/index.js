7f2b1e8695f8565cfde2d11108f1c02345a72df2
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2021-07-09 01:02:17 -0400

    idk what happened

--- express-todo/index.js
+++ express-todo/index.js
@@ -15,8 +15,16 @@ app.get('/', (req, res) => {
   res.render('../lobby/lobby')
 })
 
-app.post('/redirect', (req,res) => {
-  res.redirect("/Todo")
+app.post('/redirect', (req, res) => {
+  res.redirect(301, "/Todo")
+})
+
+app.post('/Todo', (req, res) => {
+  const {name, description} = req.body
+  todo.push({
+    name,
+    description
+  })
 })
 
 app.get('/Todo', (req, res) => {
